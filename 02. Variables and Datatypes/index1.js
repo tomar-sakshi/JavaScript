d5305ed8f457286c7995ac2547826423a9d9c95d
ch-sakshi <ch.sakshi005@gmail.com>
2025-12-29 12:51:58 +0530

    add: variable declaration and initiliazation

---
+++ 02. Variables and Datatypes/index1.js
@@ -0,0 +1,15 @@
+// How to create variables.
+
+let name ="Sakshi Tomar";
+
+let age = 26;
+age = 27;
+
+console.log(name, age);
+
+const account = 12345;
+// account =34556; // const variables can not be redeclared, this line will throw error.
+
+console.log(account);
+
+
