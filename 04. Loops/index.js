618af08b3964fa172ceca926d10729f0b639cf66
ch-sakshi <ch.sakshi005@gmail.com>
2026-01-26 23:59:16 +0530

    add: loops

--- 04. Loops/index.js
+++ 04. Loops/index.js
@@ -1,5 +1,75 @@
 //Loops
 
-for(let i = 0; i < 10; i++){
-  console.log(i);
-}
+// for(let i = 2; i <= 10; i=i+2){
+//   console.log(i + "*");
+// }
+
+// While Loop
+// let i = 1;
+// while (i < 3) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do-While Loop
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 3);
+
+// if else condition
+
+// let age = 25;
+
+// if (age >= 18) {
+//   console.log("Eligible for vote");
+// } else {
+//   console.log("Not Eligible for vote");
+// }
+
+// let age = 35;
+
+// if (age < 18) {
+//   console.log("Kid");
+// } else if (age >= 60) {
+//   console.log("old");
+// } else {
+//   console.log("Young");
+// }
+
+// Logical Operator
+
+// And Operator (&&) -If first value is false, it will return the first value itself.
+// If first value is true, it will return second value.
+
+// console.log(true&&true);//true
+// console.log(true&&false);//false
+// console.log(false&&false);//false
+// console.log(false&&true);//false
+
+// OR Operator (||)-If first value is true, it will return the first value itself.
+// If first value is false, it will return second value.
+
+// console.log(true||true);//true
+// console.log(true||false);//true
+// console.log(false||false);//false
+// console.log(false||true);//true
+
+// let a = "Suryanshu";
+// let b = "Sakshi";
+// console.log(a&&b);//Sakshi
+
+// let a = 0;
+// let b = 30;
+// console.log(a && b);// 0
+
+// let a = 0;
+// let b = 20;
+// console.log(a||b);//20
+
+// Not Operator (!)
+
+let a =30;
+let b = 30.0;
+let c = a === b;
