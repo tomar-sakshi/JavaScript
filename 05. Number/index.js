53095efd5482013a1f02c3acb115d983af6a5fc9
ch-sakshi <ch.sakshi005@gmail.com>
2026-01-27 23:48:49 +0530

    add : new line

--- 05. Number/index.js
+++ 05. Number/index.js
@@ -46,6 +46,26 @@
 // [0,1] : It will generate value between 0 and 1, where 0 is included but 1 is not included
 
 //Satebajii ek game banata:(0-99)
-// let randomNoFloat = Math.random()*100;
-// let randomNo = Math.floor(randomNoFloat);
+// const randomNoFloat = Math.random()*100;
+// const randomNo = Math.floor(randomNoFloat);
 // console.log(randomNo);
+
+
+// const randomNoFloat = Math.random()*6;
+// const randomNo = Math.floor(randomNoFloat);
+// console.log(randomNo,randomNo+1);
+
+// Math.floor(Math.random()*totalNumberOfOutcome)+shift
+
+// 15-25
+// const randomNoFloat = Math.random()*11;
+// const randomNo = Math.floor(randomNoFloat); 
+// console.log(randomNo,randomNo+15);
+
+
+// Math.floor(Math.random()*(max-min+1))+min
+
+// OTP Generate: 4 digit:1000-9999
+
+
+// console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
