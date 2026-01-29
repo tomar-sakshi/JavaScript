562d52da51696ede4030458c199aced7e4c82e76
ch-sakshi <ch.sakshi005@gmail.com>
2026-01-29 18:27:48 +0530

    Refactor: Number and Math

---
+++ 06. Maths/index.js
@@ -0,0 +1,31 @@
+// Maths
+// console.log(Math.floor(5.9));//5
+// console.log(Math.max(56,44,67,89890,59));//89890
+// console.log(Math.min(56,34,12,2, 0, -20, 20));
+// console.log(Math.abs(-8));
+// console.log(Math.ceil(7.8));
+// console.log(Math.floor(6.4));
+// console.log(Math.random());
+// [0,1] : It will generate value between 0 and 1, where 0 is included but 1 is not included
+
+//Satebajii ek game banata:(0-99)
+// const randomNoFloat = Math.random()*100;
+// const randomNo = Math.floor(randomNoFloat);
+// console.log(randomNo);
+
+// const randomNoFloat = Math.random()*6;
+// const randomNo = Math.floor(randomNoFloat);
+// console.log(randomNo,randomNo+1);
+
+// Math.floor(Math.random()*totalNumberOfOutcome)+shift
+
+// 15-25
+// const randomNoFloat = Math.random()*11;
+// const randomNo = Math.floor(randomNoFloat);
+// console.log(randomNo,randomNo+15);
+
+// Math.floor(Math.random()*(max-min+1))+min
+
+// OTP Generate: 4 digit:1000-9999
+
+// console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
