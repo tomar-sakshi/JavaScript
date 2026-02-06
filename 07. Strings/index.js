5b35568124ced611d7bc5d9d91c813050b183c37
ch-sakshi <ch.sakshi005@gmail.com>
2026-02-06 20:01:37 +0530

    add: strings

---
+++ 07. Strings/index.js
@@ -0,0 +1,82 @@
+//Strings
+
+// const str1 = "Sakshi\n";
+// const str2 = 'Sakshi\nTomar';
+// const str3 = `Strike is 
+// coming soon`;
+// console.log(str1,str2,str3);
+
+// const name = "Sakshi Tomar";
+// const age = 26;
+// const str4 = `My name is ${name} and I am ${age} yrs old `;
+
+// console.log(str4);
+
+// "My name is Sakshi Tomar" 
+// "I am a Software Developer"
+// "My Skills are HTML, CSS, JS and React.Js"
+
+// const myName = "Sakshi Tomar";
+// const occupation = "Software Developer";
+// const skills = "HTML, CSS, Js and React.Js";
+// const str1 = `My name is ${myName}`;
+// const str2 = `I am a ${occupation}`;
+// const str3 = `My skills are ${skills}`;
+// const result1 = `${str1} ${str2} ${str3}`;
+// const result2 = str1+str2+str3;
+// console.log(result2);
+
+// const str = `Hello Javascript`;
+// console.log(str.length);
+// console.log(str[2]);
+
+// str[4] = "H";
+// console.log(str[4]);
+
+// const str = `Hello Coder Army`;
+// const a = str.toUpperCase();
+// const b = str.toLowerCase();
+// console.log(a);
+// console.log(b);
+
+//  const str = `Hello Coder Army Coder`;
+// console.log(str.indexOf('Cod'));
+// console.log(str.lastIndexOf('Army'));
+// console.log(str.includes('Cod')); // It returns true and false.
+
+//slice string
+// console.log(str.slice(6,10));
+// console.log(str.slice(6));
+// console.log(str.slice(-2));
+
+//  const str = `Hello Coder Army Coder`;
+//  console.log(str.substring(1,5));// In substring we did not markdown negative index. 
+
+// Concentation
+// const a = "Sakshi";
+// const b = "Tomar";
+// const c = a+" "+b;// add empty string between a and b to add space. Sakshi Tomar
+// console.log(c);
+
+// console.log(10+"Sakshi")//10Sakshi
+// console.log(24+"Sakshi"+20);//24Sakshi20
+// console.log(30+50+"Sakshi");// 80Sakshi
+
+//Replace String
+
+// const str = `Hello Coder Army Coder`;
+// console.log(str.replace("llo","y"));
+// console.log(str.replaceAll("ode","rec"));
+
+// trim string
+
+// const user = " Sakshi Tomar ";
+// console.log(user.trim());
+
+// splitting string
+
+// const names = "Sakshi Suryanshu Khushi Tanu Arnima Kajal";
+// console.log(names.split(","));
+// console.log(names.split(" "));
+
+
